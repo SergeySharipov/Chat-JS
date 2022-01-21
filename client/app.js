bff3eaaceb05a51ae1ab75f4aaf5f5f77d638e4a
SergeySharipov <sharipov14@gmail.com>
2022-01-21 11:22:44 -0500

    Basic setup

--- client/app.js
+++ client/app.js
@@ -1,6 +1,13 @@
-$(document).ready(function(){
-    $("button").click(function(){
+var socket = io();
+
+$(document).ready(function () {
+    $("button").click(function () {
         var text = $('#message').val();
-        alert(text);
+        socket.emit('message', text);
+        $('#message').val('');
     });
-  });
+});
+
+socket.on('message', function (msg) {
+    $('<li>').text(msg).appendTo('#history');
+});
